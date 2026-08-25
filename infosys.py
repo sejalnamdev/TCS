1c2fdb33018f8719f9a4eb35c4fd10d8e03d8003
sejalnamdev <namdevsejal31@gmail.com>
2026-08-25 21:24:24 +0530

    infosys

--- infosys.py
+++ infosys.py
@@ -1,1 +1,23 @@
-infosys
+n = int(input())
+s = []
+for i in range(n):
+    s.append(int(input()))
+
+if n < 2:
+    print(None)
+
+else:
+
+    res = float('-inf')
+    
+
+    for i in range(n-1):
+        s[i], s[i+1] = s[i+1], s[i]
+        power = 0
+        for j in range(n):
+            power += s[j]*j
+        res = max(res,power)
+        s[i], s[i+1] = s[i+1], s[i]
+
+
+print(res)
